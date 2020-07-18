ff89d57cd331616a851ebff0acece5929f58a78e
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-07-18 08:55:45 +0530

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { ScrollView } from 'react-native'
+
+class App extends Component {
+  render() {
+    return (
+      <ScrollView>
+
+      </ScrollView>
+    )
+  }
+}
+
+export default App
